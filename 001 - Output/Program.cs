a1dfdf1fad48c6be2bce44a38ee63a5c6e061576
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-04-25 16:46:30 -0300

    🕹️ Update: C# Course

---
+++ 001 - Output/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyFirstProgram{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Qual o seu nome?");
+            string name = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Qual a sua idade?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("SEJA BEM-VINDO AO C#, " + name + "!!!");
+            Console.WriteLine("USUÁRIO: " + name);
+            Console.WriteLine("IDADE DO USUÁRIO: " + age);
+        }
+    }
+}
---
+++ 002 - Variables/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyFirstProgram{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I like pizza!!!");
+
+
+            // ================== VARIABLES
+            int x;
+            x = 10;
+
+            int y = 5;
+
+            int z = x + y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            int age = 21; // integer number
+            double height = 300.5; // decimal number
+            bool alive = true; // boolean(true or false)
+            char symbol = '@';
+            string name = "Wallace";
+            double pi = 3.14159;
+
+            Console.WriteLine("A sua idade é: " + age);
+            Console.WriteLine("Your height is: " + height + "cm");
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your symbol is: " + symbol);
+            Console.WriteLine("Your name is: " + name);
+            Console.WriteLine("Pi number is: " + pi);
+
+            string username = symbol + name;
+
+            Console.WriteLine("Your  username is: " + username);
+            // ================== END OF VARIABLES
+
+            Console.ReadKey();
+        }
+    }
+}
